2bd4b917116cbe1c3209a33ecdcdaf3cfc88ce3f
Sai Narendra <tannirusainarendra81@gmail.com>
2025-09-20 00:31:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 class EventTarget {
-    constructor() {
-      this.listeners = new Map();
-    }
-  
-    addEventListener(event, callback) {
-      
-    }
-  
-    removeEventListener(event, callback) {
-      
-    }
-  
-    dispatchEvent(event) {
-    
-    }
-    
+  constructor() {
+    // Map of eventName -> Set of callbacks
+    this.listeners = new Map();
   }
-  
+
+  addEventListener(event, callback) {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, new Set());
+    }
+    // Set automatically prevents duplicate callbacks
+    this.listeners.get(event).add(callback);
+  }
+
+  removeEventListener(event, callback) {
+    if (this.listeners.has(event)) {
+      this.listeners.get(event).delete(callback);
+
+      // Clean up if no listeners remain
+      if (this.listeners.get(event).size === 0) {
+        this.listeners.delete(event);
+      }
+    }
+  }
+
+  dispatchEvent(event) {
+    if (this.listeners.has(event)) {
+      for (const callback of this.listeners.get(event)) {
+        callback(); // invoke callback
+      }
+    }
+  }
+}
